d9291eeb17120e222e7c7193c1ce39f7b7329988
Madhukar B <tejasbondre27@gmail.com>
2026-09-07 20:16:28 +0530

    interface final commit

--- Interface.java
+++ Interface.java
@@ -21,13 +21,34 @@ public class Interface implements Vehicle {
 // all metod of interface must be defined in teh class where they are implemenetd
 // defined method must be kept in public access secifier.
 
+// later lets suppse i have another class sportsvehicle
+class Sportsvehicle implements Vehicle{
+     public void car(){
+            System.out.println("i drew sports car");
+        }
+        public void bike(){
+            System.out.println(" idrew sports bike");
+        }
+
+        public void brake(int n){
+            System.out.println("brke to spots for "+ n);
+        }
+}
+
 class Main {
     public static void main(String[] args){
-        Interface obj =  new Interface();
+        // Interface obj =  new Interface();
+        Vehicle obj = new Interface();
         obj.brake(6);
         obj.bike();
         obj.car();
+        
+        Vehicle v2 = new Sportsvehicle();
+    v2.car();
+
     }
+
+    
 }
 
 
