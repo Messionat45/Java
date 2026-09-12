2f5aa10b71f843aa35a20469c3760e2c99da0a5b
Madhukar B <tejasbondre27@gmail.com>
2026-09-12 20:08:58 +0530

    springboot api initial commit

--- springapi/src/main/java/com/example/springapi/HelloController.java
+++ springapi/src/main/java/com/example/springapi/HelloController.java
@@ -3,11 +3,11 @@ package com.example.springapi;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController 
+@RestController //tells spring it hadles http req
 public class HelloController {
      
-    @GetMapping ("/hello")
+    @GetMapping ("/hello") // maps get hellow req to a perticualr method ( here hello method is defined belwo)
     public String hello(){
-        return "Hellow from spring boot"
+        return "Hellow from spring boot";
     }
 }
